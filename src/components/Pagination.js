6fe8e44226f23a58f622994afe79a8512736d937
A Sneha <sneha04arul@gmail.com>
2026-01-08 11:34:44 +0530

    Create Pagination.js

---
+++ src/components/Pagination.js
@@ -0,0 +1,13 @@
+function Pagination({ page, setPage }) {
+  return (
+    <div className="pagination">
+      <button disabled={page === 1} onClick={() => setPage(page - 1)}>
+        Prev
+      </button>
+      <span>Page {page}</span>
+      <button onClick={() => setPage(page + 1)}>Next</button>
+    </div>
+  );
+}
+
+export default Pagination;
